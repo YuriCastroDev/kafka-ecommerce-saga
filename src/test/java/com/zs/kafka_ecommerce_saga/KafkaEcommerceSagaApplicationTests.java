936d2ecfa98afa02ed51e2c302e69d61ad575602
Yuri Castro <yuri.castro.dev@gmail.com>
2026-06-12 07:21:10 -0300

    chore: initialize spring boot project with kafka, jpa and postgresql dependencies

---
+++ src/test/java/com/zs/kafka_ecommerce_saga/KafkaEcommerceSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zs.kafka_ecommerce_saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEcommerceSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
